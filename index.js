414e6f244700fe743ca29c38b42d3c50737f524d
Svetlana Govyazina <govyazina@gmail.com>
2023-01-06 18:36:38 +0200

    html and css added

---
+++ index.js
@@ -0,0 +1,63 @@
+const data = `
+[
+    {
+        "date": "2023-01-01",
+        "explanation": "There, that dot on the right, that's the largest rock known in our Solar System. It is larger than every known asteroid, moon, and comet nucleus.  It is larger than any other local rocky planet.  This rock is so large its gravity makes it into a large ball that holds heavy gases near its surface.  (It used to be the largest known rock of any type until the recent discoveries of large dense planets orbiting other stars.)  The Voyager 1 spacecraft took the featured picture -- famously called Pale Blue Dot -- of this giant space rock in 1990 from the outer Solar System.  Today, this rock starts another orbit around its parent star, for roughly the 5 billionth time, spinning over 350 times during each trip.  Happy Gregorian Calendar New Year to all inhabitants of this rock we call Earth.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/PaleBlueDotOrig_Voyager1_960.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "The Largest Rock in our Solar System",
+        "url": "https://apod.nasa.gov/apod/image/2301/PaleBlueDotOrig_Voyager1_960.jpg"
+    },
+    {
+        "copyright": "Tunc Tezel",
+        "date": "2023-01-02",
+        "explanation": "Look up tonight and see a whole bunch of planets. Just after sunset, looking west, planets Venus, Saturn, Jupiter and Mars will all be simultaneously visible. Listed west to east, this planetary lineup will have Venus nearest the horizon, but setting shortly after the Sun.  It doesn't matter where on Earth you live because this early evening planet parade will be visible through clear skies all around the globe.  Taken late last month, the featured image captured all of these planets and more: the Moon and planet Mercury were also simultaneously visible.  Below visibility were the planets Neptune and Uranus, making this a nearly all-planet panorama. In the foreground are hills around the small village of G\u00f6k\u00e7e\u00f6ren, Ka\u015f, Turkey, near the Mediterranean coast.  Bright stars Altair, Fomalhaut, and Aldebaran are also prominent, as well as the Pleiades star cluster. Venus will rise higher in the sky at sunset as January continues, but Saturn will descend.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/AllPlanets_Tezel_1680_annotated.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "After Sunset Planet Parade",
+        "url": "https://apod.nasa.gov/apod/image/2301/AllPlanets_Tezel_1080_annotated.jpg"
+    },
+    {
+        "copyright": "Tommy Lease",
+        "date": "2023-01-03",
+        "explanation": "This line of stars is real. A little too faint to see with the unaided eye, Kemble\u2019s Cascade of stars inspires awe when seen with binoculars.  Like the Big Dipper though, Kemble\u2019s Cascade is an asterism, not a constellation. The asterism is visible in the northern sky toward the long-necked constellation of the Giraffe (Camelopardalis). This string of about 20 unrelated stars, each of similar brightness, spans over five times the angular width of the full moon. Stretching diagonally from the upper left to the lower right, Kemble's Cascade was popularized last century by astronomy enthusiast Lucian Kemble.  The bright object near the top left of the image is the relatively compact Jolly Roger open cluster of stars, officially designated as NGC 1502.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/KembleCascade_Lease_3668.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Kemble\u2019s Cascade of Stars",
+        "url": "https://apod.nasa.gov/apod/image/2301/KembleCascade_Lease_960.jpg"
+    },
+    {
+        "copyright": "Mike Selby",
+        "date": "2023-01-04",
+        "explanation": "Can a gas cloud eat a galaxy?  It's not even close.  The \\"claw\\" of this odd looking \\"creature\\" in the featured photo is a gas cloud known as a cometary globule.  This globule, however, has ruptured.  Cometary globules are typically characterized by dusty heads and elongated tails.  These features cause cometary globules to have visual similarities to comets, but in reality they are very much different.  Globules are frequently the birthplaces of stars, and many show very young stars in their heads. The reason for the rupture in the head of this object is not yet known. The galaxy to the left of the globule is huge, very far in the distance, and only placed near CG4 by chance superposition.   Discovery + Outreach: Graduate student research position open for APOD",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/cg4_selby_5430.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "CG4: The Globule and the Galaxy",
+        "url": "https://apod.nasa.gov/apod/image/2301/cg4_selby_960.jpg"
+    },
+    {
+        "copyright": "Stefan Thrun",
+        "date": "2023-01-05",
+        "explanation": "Hurtling through a cosmic dust cloud a mere 400 light-years away, the lovely Pleiades or Seven Sisters open star cluster is well-known for its striking blue reflection nebulae. It lies in the night sky toward the constellation Taurus and the Orion Arm of our Milky Way galaxy. The sister stars are not related to the dusty cloud though. They just happen to be passing through the same region of space. Known since antiquity as a compact grouping of stars, Galileo first sketched the star cluster viewed through his telescope with stars too faint to be seen by eye. Charles Messier recorded the position of the cluster as the 45th entry in his famous catalog of things which are not comets. In Greek myth, the Pleiades were seven daughters of the astronomical titan Atlas and sea-nymph Pleione. Their parents names are included in the cluster's nine brightest stars. This well-processed, color-calibrated telescopic image features pin-point stars and detailed filaments of interstellar dust captured in over 9 hours of exposure. It spans more than 20 light-years across the Pleiades star cluster.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/M_45_Plejarden_Stefan_Thrun_klein4096.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Messier 45: The Daughters of Atlas and Pleione",
+        "url": "https://apod.nasa.gov/apod/image/2301/M_45_Plejarden_Stefan_Thrun_klein1024.jpg"
+    },
+    {
+        "copyright": "Niveth Kumar",
+        "date": "2023-01-06",
+        "explanation": "The first Full Moon of 2023 is in the sky tonight opposite the Sun at 23:08 UTC. Big and beautiful, the Moon at its brightest phase should be easy to spot. Still, for quick reference images captured near the times of all the full moons of 2022 are aranged in this dedicated astro-imaging project from Sri Lanka, planet Earth. The day, month, and a traditional popular name for 2022's twelve full moons are given in the chart. The apparent size of each full moon depends on how close the full lunar phase is to perigee or apogee, the closest or farthest point in the Moon's elliptical orbit. Like the 2022 Wolf Moon at the 1 o'clock position, tonight's Full Moon occurs within a about two days of apogee. But unlike in 2022, the year 2023 will have 13 full moons that won't all fit nicely on the twelve hour clock.",
+        "hdurl": "https://apod.nasa.gov/apod/image/2301/MoonOClock.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Moon O'Clock 2022",
+        "url": "https://apod.nasa.gov/apod/image/2301/MoonOClock1024.jpg"
+    }
+]`
+
